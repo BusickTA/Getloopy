f1f461fe1c1bd59502b2c50d88b0ff211776cff5
thomaa <busickta@mail.uc.edu>
2024-10-13 20:49:30 -0400

    get loopy

--- src/dicerollgetloopy.java
+++ src/dicerollgetloopy.java
@@ -1,25 +1,34 @@
 import java.util.Random;
+import java.util.Scanner;
+
 public class dicerollgetloopy {
     public static void main(String[] args) {
-
+        Scanner scanner = new Scanner(System.in);
         Random rand = new Random();
+        String playAgain;
 
-        int die1=0;
-        int die2=1;
-        int die3=2;
-        int counter=0;
+        do {
+            int die1 = 0;
+            int die2 = 1;
+            int die3 = 2;
+            int counter = 0;
 
+            System.out.println("Roll  Die1  Die2  Die3  Sum");
+            System.out.println("______________________________");
 
-        System.out.println("Roll Die1   Die2   Die3   Sum");
-        System.out.println("______________________________");
+            while (!(die1 == die2 && die2 == die3)) {
+                counter++;
+                die1 = rand.nextInt(1, 7);
+                die2 = rand.nextInt(1, 7);
+                die3 = rand.nextInt(1, 7);
+                int sum = die1 + die2 + die3;
+                System.out.println(counter + "     " + die1 + "      " + die2 + "      " + die3 + "     " + sum);
+            }
 
-        while (!(die1 == die2 && die2 == die3)){
-            counter++;
-            die1=rand.nextInt(1,7);
-            die2=rand.nextInt(1,7);
-            die3=rand.nextInt(1,7);
-            int sum =die1+die2+die3;
-            System.out.println(counter + "     "+ die1 + "      "+ die2 + "      "+ die3 + "     "+ sum);
-        }
-    }
-}
+            System.out.print("Do you want to play again? (Y/N): ");
+            playAgain = scanner.nextLine();
+        } while (playAgain.equalsIgnoreCase("Y"));
+
+        scanner.close();
+        System.out.println("Thank you for playing!");
+    }}
